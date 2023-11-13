ac9a8d0754951e74b614395445f71754122a8fd6
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-11-13 15:18:55 -0300

    as threads são criadas em um arranjo aleatório agora

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ import ReadWriterService.WritersService;
 import java.io.FileNotFoundException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 public class Main {
@@ -12,6 +15,7 @@ public class Main {
     static ArrayList<String> contentFile = new ArrayList<String>();
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        //Salva os dados do arquivo txt em uma lista, onde cada indice contém uma palavra (com pontuação satélite, quando houver)
         contentFile = FileService.readContentFile("bd.txt");
 
         int readersQuantity, writersQuantity, loopController;
@@ -22,19 +26,52 @@ public class Main {
 
         while(loopController < 100) {
 
+            //define um contador das threads
             CountDownLatch latch = new CountDownLatch(100);
 
+            //cria leitores e escritores de acordo com a proporção determinada em determinado instante
             ArrayList<ReadersService> readers = ReadersService.createReaders(readersQuantity, contentFile);
             ArrayList<WritersService> writers = WritersService.createWriters(writersQuantity, contentFile);
 
-            ArrayList<Thread> threadsList = new ArrayList<Thread>();
+            //Cria um arranjo de threads
+            Thread[] threadsList = new Thread[100];
+
+            //cria um conjunto para rastrear posições que já estejam ocupadas no arranjo
+            Set<Integer> busyPositions = new HashSet<>();
+
+
+            Random random = new Random();
+
+            int randomIndex;
+
+            //Adiciona os readers e writers ao array de threads
 
             for(ReadersService readerInArray: readers){
-                threadsList.add(new Thread(readerInArray));
+
+                //Procura um espaço aleatório do array que esteja desocupado
+                do {
+                    randomIndex = random.nextInt(100);
+                } while (busyPositions.contains(randomIndex));
+                //Adiciona o elemento ao array
+                threadsList[randomIndex] = new Thread(readerInArray);
+                busyPositions.add(randomIndex);
+
             }
+
             for(WritersService writerInArray: writers){
-                threadsList.add(new Thread(writerInArray));
+
+                //Procura um espaço aleatório do array que esteja desocupado
+                do {
+                    randomIndex = random.nextInt(100);
+                } while (busyPositions.contains(randomIndex));
+                //Adiciona o elemento ao array
+                threadsList[randomIndex] = new Thread(writerInArray);
+                //adiciona o index ao conjunto de ocupados
+                busyPositions.add(randomIndex);
+
             }
+
+
 
 
             long timeStart = System.currentTimeMillis();
